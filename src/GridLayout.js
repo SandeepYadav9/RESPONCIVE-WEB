145a38b6d9f110c8d24c962c3ca12724c473e438
SandeepYadav9 <sandeepuim10@gmail.com>
2022-02-12 00:16:17 +0530

    RESPONCIVE

---
+++ src/GridLayout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./GridLayout.css";
+function GridLayout() {
+  return (
+    <div className="container">
+      <div className="items item-1-container item-1 ">One-1</div>
+
+      <div className="items item-2">TWO- 2</div>
+      <div className="items">ONE- 3</div>
+      <div className="items">TWO 4</div>
+      <div className="items">THREE 5</div>
+    </div>
+  );
+}
+export default GridLayout;
